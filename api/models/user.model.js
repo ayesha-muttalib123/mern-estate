6c1bc8c8ac36ddc7dbe716960123408f7b61075e
ayesha-muttalib123 <ayeshamutallib007@gmail.com>
2024-05-29 12:34:33 +0500

    create UserModel

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,6 +7,13 @@ const userSchema=new mongoose.Schema({
         required:true,
         unique:true
     },
+    email:{
+    
+            type:String,
+            required:true,
+            unique:true
+        
+    },
     password:{
         type:String,
         required:true,
