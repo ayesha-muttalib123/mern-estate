fe5cf5c416e125f49218f95efcc88318c993657d
ayesha-muttalib123 <ayeshamutallib007@gmail.com>
2024-05-29 11:15:09 +0500

    create and run server

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express();
+const port=3000;
+
+
+
+
+app.listen(port,()=>{
+console.log( " port is listening at "+port)
+})
+
+// actually i have created my githu repository in client folder i want tomaove it in root folder:  mv .git ../
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import SignIn from './pages/SignIn'
 import SignOut from './pages/SignOut'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './componenets/Header'
 
 export default function App() {
   return (
     
+    <>
+    <Header/>
     <Routes>
       <Route  path='/' element={<Home/>}/>
       <Route  path='/SignIn' element={<SignIn/>}/>
@@ -18,5 +21,7 @@ export default function App() {
       
 
     </Routes>
+    
+    </>
   )
 }
